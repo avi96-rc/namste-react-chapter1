4afe7360d16ef24d494a6a3b9401936aa2fcea8b
avi96_rc <avi96.rc@gmail.com>
2023-01-12 04:20:33 +0530

    ADD: git ignore

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-const heading = React.createElement('h1', {}, 'namaste react')
-const heading1 = React.createElement('h1', {}, 'heading 1')
-const heading2 = React.createElement('h2', {}, 'heading 2')
+import React from "react"
+import  ReactDOM  from "react-dom/client"
+const heading = React.createElement('h1', {key:'1'}, 'namaste react from parcel')
+const heading1 = React.createElement('h1', {key:'2'}, 'heading 1')
+const heading2 = React.createElement('h2', {key:3}, 'heading 2')
 const container = React.createElement('div', {
     id: 'container',
-    class:'ravi',
+    className:'ravi',
     pop:'ravi'
-}, [heading1, heading1])
+}, [heading,heading1, heading2])
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(container)
